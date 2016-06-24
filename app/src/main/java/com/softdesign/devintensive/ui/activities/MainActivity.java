800b3eebc59b87f7e7a53e406306afd864cd0062
miller777 <miller777@mail.ru>
2016-06-25 01:41:39 +0300

    Номе task 2 complit!

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,16 +1,81 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+
+
+    /**
+     * Метод вызывается при создании активити (после изменения конфигурации/возврата к
+     * текущей активности после ее уничтожения.
+     * <p/>
+     * В данном месте инициализируется/производится:
+     * - UI пользовательский интерфейс (статика)
+     * - инициализация статических данных активити
+     * - связь данных со списками (инициализация адаптеров)
+     * <p/>
+     * Не запускать длительные операции по работе с данными в onCreate()!
+     *
+     * @param savedInstanceState - объект со значением, сохраненным в Bundle - состояние UI
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //123
+        Log.d(TAG, "OnCreate");
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "OnResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "OnPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "OnStop");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "OnRestart");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "OnDestroy");
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
