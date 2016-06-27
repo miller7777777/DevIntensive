36cca1a8adefa1cb20bb3759343bc6e1b40d355f
miller777 <miller777@mail.ru>
2016-06-27 03:10:59 +0300

    Номе task 2 complit!
    Fix left and right paddings

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,19 +1,22 @@
 package com.softdesign.devintensive.ui.activities;
 
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+    private ImageView mCallImg;
 
 
     /**
@@ -34,6 +37,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "OnCreate");
+
+        mCallImg = (ImageView) findViewById(R.id.call_img);
+        mCallImg.setOnClickListener(this);
 
 
     }
@@ -77,5 +83,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()){
+            case R.id.call_img:
+//                showProgress();
+//                runWithDelay();
+                break;
+        }
+
     }
+
+//    private void runWithDelay() {
+//        final Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                //TODO: Выполнить с задержкой
+//                hideProgress();
+//            }
+//        }, 5000);
+//    }
 }
