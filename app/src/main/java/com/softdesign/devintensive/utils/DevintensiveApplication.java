d5201698e990751ff923740a1810e6cc2abd68b5
miller777 <miller777@mail.ru>
2016-06-29 00:18:52 +0300

    Lesson 3, текущие правки. Создан класс PreferencesManager.

--- app/src/main/java/com/softdesign/devintensive/utils/DevintensiveApplication.java
+++ app/src/main/java/com/softdesign/devintensive/utils/DevintensiveApplication.java
@@ -1,5 +1,21 @@
 package com.softdesign.devintensive.utils;
 
 
-public class DevintensiveApplication {
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class DevintensiveApplication extends Application{
+
+    public static SharedPreferences sSharedPreferences;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+    public static SharedPreferences getSharedPreferences() {
+        return sSharedPreferences;
+    }
 }
