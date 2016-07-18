60cb0a6613791fd84e78652964a89223e5b4c401
miller777 <miller777@mail.ru>
2016-07-19 00:37:57 +0300

    Немного поправил разметку из 6-го задания.
    Сильно легче не стало.

--- app/src/main/java/com/softdesign/devintensive/ui/activities/ProfileUserActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/ProfileUserActivity.java
@@ -3,6 +3,8 @@ package com.softdesign.devintensive.ui.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.softdesign.devintensive.R;
+
 public class ProfileUserActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/softdesign/devintensive/ui/activities/UserListActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/UserListActivity.java
@@ -115,5 +115,10 @@ public class UserListActivity extends AppCompatActivity {
     private void setupToolbar() {
         setSupportActionBar(mToolbar);
         ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
